04d988dbe7204a0045d31de9cde0bb132e248abf
nkhanh-deakin <nkhanh@deakin.edu.au>
2016-08-17 10:59:37 +1000

    Init

---
+++ MultiRunner/MainFrm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace MultiRunner
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.lstCommands = new System.Windows.Forms.ListBox();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.lstCPUs = new System.Windows.Forms.CheckedListBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stSum = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tWait = new System.Windows.Forms.Timer(this.components);
+            this.nWait = new System.Windows.Forms.NumericUpDown();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nWait)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(12, 83);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // lstCommands
+            // 
+            this.lstCommands.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstCommands.FormattingEnabled = true;
+            this.lstCommands.Location = new System.Drawing.Point(12, 147);
+            this.lstCommands.Name = "lstCommands";
+            this.lstCommands.Size = new System.Drawing.Size(444, 199);
+            this.lstCommands.TabIndex = 1;
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(93, 12);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(363, 121);
+            this.lstFiles.TabIndex = 2;
+            // 
+            // lstCPUs
+            // 
+            this.lstCPUs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstCPUs.FormattingEnabled = true;
+            this.lstCPUs.Location = new System.Drawing.Point(462, 12);
+            this.lstCPUs.Name = "lstCPUs";
+            this.lstCPUs.Size = new System.Drawing.Size(120, 304);
+            this.lstCPUs.TabIndex = 3;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 12);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(12, 42);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 5;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(12, 110);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 7;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stSum});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 355);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(594, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stSum
+            // 
+            this.stSum.Name = "stSum";
+            this.stSum.Size = new System.Drawing.Size(39, 17);
+            this.stSum.Text = "Ready";
+            // 
+            // tWait
+            // 
+            this.tWait.Enabled = true;
+            this.tWait.Interval = 10000;
+            this.tWait.Tick += new System.EventHandler(this.tWait_Tick);
+            // 
+            // nWait
+            // 
+            this.nWait.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.nWait.Location = new System.Drawing.Point(462, 326);
+            this.nWait.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.nWait.Name = "nWait";
+            this.nWait.Size = new System.Drawing.Size(120, 20);
+            this.nWait.TabIndex = 9;
+            this.nWait.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nWait.ThousandsSeparator = true;
+            this.nWait.Value = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nWait.ValueChanged += new System.EventHandler(this.nWait_ValueChanged);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 377);
+            this.Controls.Add(this.nWait);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lstCPUs);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.lstCommands);
+            this.Controls.Add(this.btnImport);
+            this.Name = "MainFrm";
+            this.Text = "MultiRunner";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nWait)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.ListBox lstCommands;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.CheckedListBox lstCPUs;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stSum;
+        private System.Windows.Forms.Timer tWait;
+        private System.Windows.Forms.NumericUpDown nWait;
+    }
+}
+
---
+++ MultiRunner/MainFrm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultiRunner
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+        List<string> filelist;
+        List<int> cpulist;
+        string[] current_cmdlist;
+        List<Queue<string>> queuelist;
+        List<bool> busylist;
+        int currentfile;
+
+        void FillListCPUs()
+        {
+            int count = Environment.ProcessorCount;
+            for (int i = 0; i < count; i++)
+            {
+                lstCPUs.Items.Add("CPU " + i.ToString());
+                if(i < count - 3)
+                    lstCPUs.SetItemChecked(i, true);
+            }
+        }
+
+        void Import2Queue()
+        {
+            lstFiles.SelectedIndex = currentfile;
+            string filename = filelist[currentfile];
+            List<string> cmdlist = new List<string>();
+            StreamReader reader = new StreamReader(filename);
+            while (!reader.EndOfStream)
+                cmdlist.Add(reader.ReadLine());
+            int d = (int)Math.Ceiling(1.0 * cmdlist.Count / cpulist.Count);
+            
+            for (int i = 0; i < cpulist.Count; i++)
+            {
+                for (int j = 0; j < d; j++)
+                    if(i * d + j < cmdlist.Count)
+                        queuelist[i].Enqueue(cmdlist[i * d + j]);
+            }
+        }
+
+        void RunQueue()
+        {
+            for (int i = 0; i < cpulist.Count; i++)
+            {
+                RunCmd(i);
+            }
+        }
+
+        void RunCmd(int i)
+        {
+            if (queuelist[i].Count == 0)
+                return;
+            busylist[i] = true;
+            string cpuid = Convert.ToString(1<<cpulist[i], 16);
+            string cmd = queuelist[i].Dequeue().Replace("@","@"+i);
+            current_cmdlist[i] = cmd;
+            string arg = "/c start /WAIT /AFFINITY " + cpuid + " /min cmd /c \"" + cmd + "\"";
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo("cmd.exe", arg);
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.EnableRaisingEvents = true;
+            p.Exited += P_Exited;
+            p.Start();
+        }
+
+        private void P_Exited(object sender, EventArgs e)
+        {
+            Process p = (Process)sender;
+            string cpuid = p.StartInfo.Arguments.Split(new char[] { ' ' }, 6)[4];
+            int cpu = (int)(Math.Log(Convert.ToInt32(cpuid, 16))/Math.Log(2));
+            busylist[(cpulist.IndexOf(cpu))] = false;
+
+            //did not work
+            //if (!tWait.Enabled)
+            //    tWait.Start();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            FillListCPUs();
+            filelist = new List<string>();
+            cpulist = new List<int>();
+            queuelist = new List<Queue<string>>();
+            busylist = new List<bool>();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+            filelist.Add(dlg.FileName);
+            lstFiles.Items.Add(Path.GetFileName(dlg.FileName));
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstFiles.SelectedIndex < 0)
+                return;
+            int k = lstFiles.SelectedIndex;
+            filelist.RemoveAt(k);
+            lstFiles.Items.RemoveAt(k);
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (lstFiles.Items.Count == 0)
+                return;
+            if (lstCPUs.CheckedItems.Count == 0)
+                return;
+
+            cpulist.Clear();
+            queuelist.Clear();
+            busylist.Clear();
+            lstCommands.Items.Clear();
+            foreach (int k in lstCPUs.CheckedIndices)
+            {
+                cpulist.Add(k);
+                queuelist.Add(new Queue<string>());
+                lstCommands.Items.Add("");
+                busylist.Add(false);
+            }
+            current_cmdlist = new string[cpulist.Count];
+            
+
+            currentfile = 0;
+            Import2Queue();
+            RunQueue();
+        }
+
+        bool tick_busy = false;
+        private void tWait_Tick(object sender, EventArgs e)
+        {
+            if (tick_busy) return;
+            tick_busy = true;
+            //tWait.Enabled = false;
+
+            stSum.Text = "";
+            for (int i = 0; i < cpulist.Count; i++)
+            {
+                if (busylist[i]) continue;
+                stSum.Text += i.ToString() + ", ";
+                RunCmd(i);
+            }
+
+            bool finished = true;
+            for (int i = 0; i < cpulist.Count; i++)
+            {
+                if (queuelist[i].Count != 0) finished = false;
+                string status = i.ToString("00") + ": (";
+                status += queuelist[i].Count.ToString("000") + ") : ";
+                status += current_cmdlist[i];
+                lstCommands.Items[i] = status;
+            }
+            if (finished)
+            {
+                currentfile++;
+                if (currentfile < filelist.Count)
+                {
+                    Import2Queue();
+                    RunQueue();
+                }
+            }
+
+            tick_busy = false;
+        }
+
+        private void nWait_ValueChanged(object sender, EventArgs e)
+        {
+            tWait.Interval = (int)nWait.Value;
+        }
+    }
+}
